fbd6599b6a8f9f124fe152d319bcb8d83f5645d9
65160104 <152173850+65160104@users.noreply.github.com>
2023-12-21 17:42:56 +0700

    lab3

--- lab3.php
+++ lab3.php
@@ -1,6 +1,30 @@
 <html>
-    <body>
-        <h1>This is PHP<?php echo 'Hello World!'?></h1>
-        </h1>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <?php $multi_x = 2; ?>
+    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+
+    <table>
+        <tbody>
+            <tr>
+                <td></td>
+                <td>x</td>
+                <td></td>
+                <td>=</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+    <style>
+        table {
+            width: 150px;
+            height: 265px;
+        }
+    </style>
+</body>
+
 </html>
