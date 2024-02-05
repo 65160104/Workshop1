59da94f522fb58e919f64f38ecac4d1a4ac26210
65160104 <152173850+65160104@users.noreply.github.com>
2024-02-05 17:26:57 +0700

    title

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\MyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/my-route',function(){
-    return view('myform');
-});
+// Route::get('/my-route',function(){
+//     return view('myform');
+// });
 // Route::get('/my-form',function(){
 //     $data = ['val_a' => 'Hello'];
 //     $data ['val_b'] = "Laravel";
@@ -29,3 +29,18 @@ Route::get('/my-route',function(){
 //     $data['myinput'] = $req->input('myinput');
 //     return view('myroute',$data);
 // });
+
+
+// Route::get('/login', [MyAuth::class, 'login_view'])->name('login');
+// Route::get('/register', [MyAuth::class, 'register_view']);
+// Route::get('/logout', [MyAuth::class, 'logout_process']);
+// Route::post('/login', [MyAuth::class, 'login_process']);
+// Route::post('/register', [MyAuth::class, 'register_process']);
+
+// Route::resource('titles', C_titles::class)->middleware('auth');
+// Route::middleware('auth')->group(function(){
+//     // auth first
+// });
+
+use App\Http\Controllers\C_titles;
+Route::resource('titles', C_titles::class);
